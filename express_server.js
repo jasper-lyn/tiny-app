0ebfef1a9ab67837481ea8c6c553b6c5d153b41c
Jasper Walton <jasper.lyn.walton@gmail.com>
2017-09-13 15:16:09 +0000

    debugs to see 'post' parameters and adds random string generator function

--- express_server.js
+++ express_server.js
@@ -2,7 +2,20 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs");
+
+function generateRandomString() {
+  var randomString = "";
+  var charSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (var i = 0; i < 6; i++) {
+    randomString += charSet.charAt(Math.floor(Math.random() * charSet.length));
+  }
+  return randomString;
+}
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -14,9 +27,18 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   let templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id]};
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
 });
 
 app.listen(PORT, () => {
